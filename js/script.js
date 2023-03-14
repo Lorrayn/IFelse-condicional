ff5a03e008dc1d8c0ab28c90d535f6fcab19f499
LORRAYNE <cg301519x@LAB204M14.IFSP>
2023-03-14 09:24:56 -0300

    Projeto mudar fundo finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 let button = document.getElementById('button');
+let fundo_azul = false;
+button.style.width = '120px' 
+button.style.height ='60px'
+button.style.fontSize = '16px'
 function mudar_fundo(){
-    document.body.style.background = '#40E0D0';
+    if ( fundo_azul == false){
+        fundo_azul = true;
+        document.body.style.background = '#40E0D0';
+        button.style.background = 'purple';
+        button.style.color =  'black'
+        button.style.width = '120px' 
+        button.style.height ='60px'
+        button.style.fontSize = '16px'     
+    }else{
+        fundo_azul= false;
+        document.body.style.background = 'black';
+        button.style.background = 'purple';
+        button.style.color = 'black';
+        button.style.fontSize = '16px'
+    }
+    
 }
